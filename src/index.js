5a42409ea3ebeb37a891114b84a3620ce05c844c
Johannes Loewe <johannes@loewe.pm>
2017-06-21 14:29:21 +0200

    added more projects and changed project layout

--- src/index.js
+++ src/index.js
@@ -2,9 +2,15 @@ require("./js/nav.js");
 require("./html/index.html");
 require("./sass/main.scss");
 
+require("./img/johannes-loewe-portrait.jpg");
+
 require("./img/logos/nodejs.png");
 require("./img/logos/express.png");
 require("./img/logos/sqlite.png");
 require("./img/logos/vue.png");
+require("./img/logos/mongo.png");
+require("./img/logos/react.png");
 
 require("./img/screens/voto.png");
+require("./img/screens/stallforth.png");
+require("./img/screens/feuertruppe.png");
