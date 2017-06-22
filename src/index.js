74e4fda741c263b8fcdf2e478928d2db80ec03d2
Johannes Loewe <johannes@loewe.pm>
2017-06-22 18:51:50 +0200

    added logos for skills

--- src/index.js
+++ src/index.js
@@ -10,7 +10,19 @@ require("./img/logos/express.png");
 require("./img/logos/sqlite.png");
 require("./img/logos/vue.png");
 require("./img/logos/mongo.png");
-require("./img/logos/react.png");
+require("./img/logos/html.png");
+require("./img/logos/css.png");
+require("./img/logos/js.png");
+require("./img/logos/webpack.png");
+require("./img/logos/sass.png");
+require("./img/logos/java.png");
+require("./img/logos/git.png");
+require("./img/logos/wordpress.png");
+require("./img/logos/pug.png");
+require("./img/logos/linux.png");
+require("./img/logos/mocha.png");
+require("./img/logos/sass.png");
+
 
 require("./img/screens/voto.png");
 require("./img/screens/stallforth.png");
